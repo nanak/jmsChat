c866c914ce2f1842be1d3a8b1bd02288cdfee68b
Nanak Tattyrek <nanak@tattyrek.at>
2013-05-03 11:46:55 +0200

    Start JMSChatRun

--- JMSChatStart.java
+++ JMSChatStart.java
@@ -7,23 +7,25 @@ package jmsChat;
 public class JMSChatStart {
 	private static String message_broker_ip;
 	private static String username;
-	private static String chatroom;
+	private static String subject;
 
 	/**
 	 * Takes three arguments:
 	 * 	<message_broker_ip> ... ip of the message broker
 	 * 	<username> ............ name you want to use in the chatroom
-	 * 	<chatroom> ............ chatroom you want to connect to
+	 * 	<subject> ............. chatroom you want to connect to
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
 		if (args.length == 3) {
 			message_broker_ip = args[0];
 			username = args[1];
-			chatroom = args[2];
+			subject = args[2];
 		}
 		if (args.length != 3) {
-			System.out.println("Synopsis: jmschat <message_broker_ip> <username> <password>");
+			System.out.println("Synopsis: jmschat <message_broker_ip> <username> <chatroom>");
+			System.exit(0);
 		}
+		JMSChatRun jmsc = new JMSChatRun(message_broker_ip, username, subject);
 	}
 }
