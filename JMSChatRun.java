a141aa70eb94596241b772f4eb47ec8c92099a6e
Nanak Tattyrek <nanak@tattyrek.at>
2013-05-03 12:23:35 +0200

    exit message

--- JMSChatRun.java
+++ JMSChatRun.java
@@ -4,8 +4,6 @@
  */
 package jmsChat;
 
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import static java.lang.Thread.MIN_PRIORITY;
@@ -23,86 +21,88 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-
 /**
- *
+ * 
  * @author Paradox
  */
-public class JMSChatRun extends Thread{
+public class JMSChatRun extends Thread {
 
-  private static String user = null;
-  private static String password = ActiveMQConnection.DEFAULT_PASSWORD;
-  private static String url = null;//"failover://tcp://192.168.1.101:61616";
-  private static String subject = null;//"VSDBChat";
-  private boolean go = true;
-    
-    public JMSChatRun (String ip, String user, String subject){
-        this.user=user;
-        this.url="failover://tcp://"+ip+":61616";
-        this.subject=subject;
-        this.start();
-        
-        
-    }
-    
-    @Override
-    public void run() {
-        Session session = null;
-	Connection connection = null;
-	MessageConsumer consumer = null;
-	Destination destination = null;
-	MessageProducer producer = null;
-        try {
-            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(user, password, url);
-            connection = connectionFactory.createConnection();
-            connection.start();
-            // Create the session
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            destination = session.createTopic( subject );
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
-            destination = session.createTopic( subject );
-		  
-            // Create the producer.
-            producer = session.createProducer(destination);
-            producer.setDeliveryMode( DeliveryMode.PERSISTENT );
-            while (go){
-		// Create the consumer
-		consumer = session.createConsumer( destination );
-		// Start receiving
-		TextMessage message = (TextMessage) consumer.receive(300);
-                if ( message != null ) {
-                    System.out.println(message.getText() );
-                    message.acknowledge();
-                 }
-                 if(br.ready()){
-                     String m = br.readLine();
-                     if(!m.startsWith("/exit")){
-                         m = user+ "@" +InetAddress.getLocalHost()+": "+m;
-                        TextMessage messageSend = session.createTextMessage(m);
-                        producer.send(messageSend);
-                     }else{
-                         go=false;
-                     }
-                 }
-            }
-         } catch (Exception e) {
-	      System.out.println("[MessageConsumer] Caught: " + e);
-	      e.printStackTrace();
-	} finally {
-		try { consumer.close(); } catch ( Exception e ) {}
-		try { session.close(); } catch ( Exception e ) {}
-		try { connection.close(); } catch ( Exception e ) {}
+	private static String user = null;
+	private static String password = ActiveMQConnection.DEFAULT_PASSWORD;
+	private static String url = null;// "failover://tcp://192.168.1.101:61616";
+	private static String subject = null;// "VSDBChat";
+	private boolean go = true;
+
+	public JMSChatRun(String ip, String user, String subject) {
+		this.user = user;
+		this.url = "failover://tcp://" + ip + ":61616";
+		this.subject = subject;
+		this.start();
+
 	}
-  
-        
-        
-		
-    
-    
-    }
-    
-    
-    
-    
+
+	@Override
+	public void run() {
+		Session session = null;
+		Connection connection = null;
+		MessageConsumer consumer = null;
+		Destination destination = null;
+		MessageProducer producer = null;
+		try {
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+					user, password, url);
+			connection = connectionFactory.createConnection();
+			connection.start();
+			// Create the session
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			destination = session.createTopic(subject);
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+
+			destination = session.createTopic(subject);
+
+			// Create the producer.
+			producer = session.createProducer(destination);
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			while (go) {
+				// Create the consumer
+				consumer = session.createConsumer(destination);
+				// Start receiving
+				TextMessage message = (TextMessage) consumer.receive(300);
+				if (message != null) {
+					System.out.println(message.getText());
+					message.acknowledge();
+				}
+				if (br.ready()) {
+					String m = br.readLine();
+					if (!m.startsWith("/exit")) {
+						m = user + "@" + InetAddress.getLocalHost() + ": " + m;
+						TextMessage messageSend = session.createTextMessage(m);
+						producer.send(messageSend);
+					} else {
+						System.out.println("Chat stopped");
+						go = false;
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("[MessageConsumer] Caught: " + e);
+			e.printStackTrace();
+		} finally {
+			try {
+				consumer.close();
+			} catch (Exception e) {
+			}
+			try {
+				session.close();
+			} catch (Exception e) {
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+			}
+		}
+
+	}
+
 }
